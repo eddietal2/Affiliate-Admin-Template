8fd7af18cbf08a6b6006950d81176acffda0569c
Eddie <eddielacrosse2@gmail.com>
2022-03-12 21:11:45 -0500

    Pluggin in Admin Landing Page API

--- src/app/pages/landing-page/landing-page.page.ts
+++ src/app/pages/landing-page/landing-page.page.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { LandingPageService } from 'src/app/services/landing-page/landing-page.service';
+
+interface LandingPageInfo {
+  _id: string,
+  welcomeMessage: string,
+  sample: string,
+  featuredProducts: Array<String>,
+  whyHypnosis: string,
+  membershipMessage: string,
+}
 
 @Component({
   selector: 'app-landing-page',
@@ -7,16 +19,370 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPagePage implements OnInit {
 
-  constructor() { }
+
+  constructor(
+    private landingPageService: LandingPageService,
+    private alertController: AlertController
+  ) { }
 
   ngOnInit() {
+    this.getLandingPageInfo();
+  }
+
+  @HostListener('unloaded')
+  ngOnDestroy() {
+    this.getLandingPageInfoSub.unsubscribe();
+  }
+
+
+
+  getLandingPageInfoSub: Subscription;
+  landingPageInfo: LandingPageInfo;
+  landingPageInfo$ = new BehaviorSubject([]);
+  id: string;
+
+  /**
+   * Get Landing Page Information 
+   */
+  getLandingPageInfo() {
+    this.getLandingPageInfoSub = this.landingPageService.getAllProducts()
+      .subscribe((info) => {
+
+        this.landingPageInfo$.next(info['landingPageInfo'][0]);
+        this.id = info['landingPageInfo'][0]._id;
+      })
+  }
+
+  /**
+   * Track the Input of Welcome Message Input
+   */
+  welcomeMessageInputCount = 0;
+  welcomeMessageInputEvent(e: Event) {
+   this.welcomeMessageInputCount++;
+
+   if(this.welcomeMessageInputCount > 0) {
+     this.disableWelomeMessageInput = true;
+   }
+   // console.log(e);
+   
+  }
+
+  disableWelomeMessageInput = false;
+  welcomeMessageSpinner = false;
+  /**
+   * 
+   */
+  async editWelcomeMessage(input) {
+     console.log(input);
+     const alert = await this.alertController.create({
+       cssClass: 'edit-landing-page-alert',
+       header: 'Edit Welcome Message',
+       message: 'Are you sure?',
+       buttons: [
+         {
+           text: 'Cancel',
+           role: 'cancel',
+           cssClass: 'secondary',
+           id: 'cancel-button',
+           handler: (blah) => {
+             console.log('Confirm Cancel: blah');
+           }
+         }, {
+           text: 'Okay',
+           cssClass: 'confirm-button',
+           handler: () => {
+             console.log('Confirm Okay');
+             this.welcomeMessageSpinner = true;
+             this.landingPageService.editWelcomeMessageHTTP(this.id, input.value)
+              .subscribe((updatedLandingPageInfo: any) => {
+
+                this.landingPageInfo$.next(updatedLandingPageInfo);
+                this.welcomeMessageInputCount = 0;
+                this.disableWelomeMessageInput = false;
+                
+
+                console.clear();
+                console.log(updatedLandingPageInfo);
+                console.log(this.landingPageInfo$.value);
+
+
+                setTimeout(() => {
+                  this.welcomeMessageSpinner = false;    
+                }, 800);
+
+              })
+           }
+         }
+       ]
+     });
+ 
+     await alert.present();
+     
+   }
+
+  /**
+   * 
+   * @param input 
+   * @param button 
+   */
+  async cancelEditWelcomeMessage(input, button) {
+     console.log(input);
+     console.log(button);
+     input.value = this.landingPageInfo$.value['welcomeMessage'];
+     setTimeout(() => {
+      this.disableWelomeMessageInput = false;
+     }, 0);
+     
+   }
+
+  /**
+   * Track the Input of Welcome Message Input
+   */
+  sampleInputCount = 0;
+  sampleInputEvent(e: Event) {
+   this.sampleInputCount++;
+
+   if(this.sampleInputCount > 0) {
+     this.disableSampleInput = true;
+   }
+   // console.log(e);
+   
+  }
+
+  disableSampleInput = false;
+  sampleSpinner = false;
+
+  /**
+   * 
+   */
+  async editSample(input) {
+    const alert = await this.alertController.create({
+      cssClass: 'edit-landing-page-alert',
+      header: 'Edit Sample',
+      message: 'Are you sure?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          id: 'cancel-button',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Okay',
+          cssClass: 'confirm-button',
+          handler: () => {
+            console.log('Confirm Okay');
+            this.sampleSpinner = true;
+            this.landingPageService.editSampleHTTP(this.id, input.value)
+             .subscribe((updatedLandingPageInfo: any) => {
+
+               this.landingPageInfo$.next(updatedLandingPageInfo);
+               this.sampleInputCount = 0;
+               this.disableSampleInput = false;
+               
+               console.log(updatedLandingPageInfo);
+               console.log(this.landingPageInfo$.value);
+               console.log(input.value);
+
+
+               setTimeout(() => {
+                 this.sampleSpinner = false;    
+               }, 800);
+
+             })
+          }
+        }
+      ]
+    });
+ 
+    await alert.present();
+   }
+
+
+  /**
+   * 
+   * @param input 
+   * @param button 
+   */
+  async cancelEditSample(input, button) {
+    console.log(input);
+    console.log(button);
+    input.value = this.landingPageInfo$.value['sampleTrack'];
+    setTimeout(() => {
+     this.disableSampleInput = false;
+    }, 0);
+    
   }
 
   /**
    * 
    */
-   editWelcomeMessage(input) {
-     console.log(input);
+  async editFeaturedProducts() {
+     console.log();
      
    }
+
+   /**
+    * Track the Input of Welcome Message Input
+    */
+   whyHypnosisInputCount = 0;
+   whyHypnosisInputEvent(e: Event) {
+    this.whyHypnosisInputCount++;
+ 
+    if(this.whyHypnosisInputCount > 0) {
+      this.disableWhyHypnosisInput = true;
+    }
+    // console.log(e);
+    
+   }
+ 
+   disableWhyHypnosisInput = false;
+   whyHypnosisSpinner = false;
+  /**
+   * 
+   */
+  async editWhyHypnosis(input) {
+    const alert = await this.alertController.create({
+      cssClass: 'edit-landing-page-alert',
+      header: 'Edit Why Hypnosis',
+      message: 'Are you sure?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          id: 'cancel-button',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Okay',
+          cssClass: 'confirm-button',
+          handler: () => {
+            console.log('Confirm Okay');
+            this.whyHypnosisSpinner = true;
+            this.landingPageService.editWhyHypnosisHTTP(this.id, input.value)
+             .subscribe((updatedLandingPageInfo: any) => {
+
+               this.landingPageInfo$.next(updatedLandingPageInfo);
+               this.whyHypnosisInputCount = 0;
+               this.disableWhyHypnosisInput = false;
+               
+               console.log(updatedLandingPageInfo);
+               console.log(this.landingPageInfo$.value);
+               console.log(input.value);
+
+
+               setTimeout(() => {
+                 this.whyHypnosisSpinner = false;    
+               }, 800);
+
+             })
+          }
+        }
+      ]
+    });
+ 
+    await alert.present();
+     
+   }
+
+   /**
+    * 
+    * @param input 
+    * @param button 
+    */
+   async cancelEditWhyHypnosis(input) {
+     console.log(input);
+     console.log(this.landingPageInfo$.value);
+     input.value = this.landingPageInfo$.value['whyHypnosis'];
+     setTimeout(() => {
+      this.disableWhyHypnosisInput = false;
+     }, 0);
+     
+   }
+
+  
+
+   /**
+    * Track the Input of Welcome Message Input
+    */
+    membershipMessageInputCount = 0;
+    membershipMessageInputEvent(e: Event) {
+     this.membershipMessageInputCount++;
+  
+     if(this.membershipMessageInputCount > 0) {
+       this.disableMembershipMessageInput = true;
+     }
+     // console.log(e);
+     
+    }
+  
+    disableMembershipMessageInput = false;
+    membershipMessageSpinner = false;
+   /**
+    * 
+    */
+   async editMembershipMessage(input) {
+    const alert = await this.alertController.create({
+      cssClass: 'edit-landing-page-alert',
+      header: 'Edit Membership Message',
+      message: 'Are you sure?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          id: 'cancel-button',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Okay',
+          cssClass: 'confirm-button',
+          handler: () => {
+            console.log('Confirm Okay');
+            this.membershipMessageSpinner = true;
+            this.landingPageService.editMembershipHTTP(this.id, input.value)
+             .subscribe((updatedLandingPageInfo: any) => {
+
+               this.landingPageInfo$.next(updatedLandingPageInfo);
+               this.membershipMessageInputCount = 0;
+               this.disableMembershipMessageInput = false;
+               
+               console.log(updatedLandingPageInfo);
+               console.log(this.landingPageInfo$.value);
+               console.log(input.value);
+
+
+               setTimeout(() => {
+                 this.membershipMessageSpinner = false;    
+               }, 800);
+
+             })
+          }
+        }
+      ]
+    });
+ 
+    await alert.present();
+   }
+   /**
+    * 
+    * @param input 
+    * @param button 
+    */
+   async cancelEditMembershipMessage(input) {
+     console.log(input);
+     console.log(this.landingPageInfo$.value);
+     input.value = this.landingPageInfo$.value['membershipMessage'];
+     setTimeout(() => {
+      this.disableMembershipMessageInput = false;
+     }, 0);
+     
+   }
+
+   
 }
--- src/app/services/landing-page/landing-page.service.ts
+++ src/app/services/landing-page/landing-page.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LandingPageService {
+  BACKEND_URL = environment.url;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient) { }
+
+    /**
+     * Get Landing Page Info HTTP Request
+     */
+    getAllProducts() {
+      return this.http.get(`${this.BACKEND_URL}/landing-page/get-landing-page-info`)
+    }
+
+    /**
+     * Edit Welcome Message HTTP Request
+     */
+     editWelcomeMessageHTTP(id: string, input: string) {
+      return this.http.post(`${this.BACKEND_URL}/landing-page/edit-welcome-message`, {id: id, newWelcomeMessage: input})
+    }
+
+    /**
+     * Edit Sample HTTP Request
+     */
+     editSampleHTTP(id: string, input: string) {
+      return this.http.post(`${this.BACKEND_URL}/landing-page/edit-sample`, {id: id, newSample: input})
+    }
+
+    /**
+     * Edit Why Hypnosis HTTP Request
+     */
+     editWhyHypnosisHTTP(id: string, input: string) {
+      return this.http.post(`${this.BACKEND_URL}/landing-page/edit-why-hypnosis`, {id: id, newWhyHypnosis: input})
+    }
+
+    /**
+     * Edit Why Hypnosis HTTP Request
+     */
+     editMembershipHTTP(id: string, input: string) {
+      return this.http.post(`${this.BACKEND_URL}/landing-page/edit-membership-message`, {id: id, newMembershipMessage: input})
+    }
 }
